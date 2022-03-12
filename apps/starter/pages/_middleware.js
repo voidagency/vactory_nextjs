b5973cff686c169498ce094ee41db19efe031baa
Hamza Bahlaouane <h.bahlaouane@void.fr>
2022-03-12 10:27:42 +0100

    fix(middleware): missing next config runtime

--- apps/starter/pages/_middleware.js
+++ apps/starter/pages/_middleware.js
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server"
-import { getDefaultLanguage } from "@vactory/next"
+// import { getDefaultLanguage } from "@vactory/next"
 
-const DEFAULT_LANGUAGE = getDefaultLanguage()
 const PUBLIC_FILE = /\.(.*)$/
 
 const stripDefaultLocale = (str) => {
@@ -10,6 +9,9 @@ const stripDefaultLocale = (str) => {
 }
 
 export function middleware(request) {
+  // @todo: middleware doesn't know about next config runtime
+  // split i18n object and make it in own file
+  // const DEFAULT_LANGUAGE = getDefaultLanguage()
   const url = request.nextUrl.clone()
   const shouldHandleLocale =
     !PUBLIC_FILE.test(url.pathname) &&
@@ -17,9 +19,9 @@ export function middleware(request) {
     url.locale === "default"
 
   const path = stripDefaultLocale(url.pathname)
-  url.pathname = `/${DEFAULT_LANGUAGE}${path}`
+  url.pathname = `/fr${path}`
   if (path === "/") {
-    url.pathname = `/${DEFAULT_LANGUAGE}`
+    url.pathname = `/fr`
   }
 
   return shouldHandleLocale ? NextResponse.redirect(url) : undefined
