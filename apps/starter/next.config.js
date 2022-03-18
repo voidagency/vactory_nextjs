0ed96b26cd908422b1c0aec0d1bdc72bbfc04a65
Hamza Bahlaouane <h.bahlaouane@void.fr>
2022-03-18 18:54:32 +0100

    fix: image styles

--- apps/starter/next.config.js
+++ apps/starter/next.config.js
@@ -19,7 +19,7 @@ const nextConfig = {
 		domains: ["localhost"],
 		dangerouslyAllowSVG: true,
 		// contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-		deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+		deviceSizes: [640, 750, 328, 828, 1080, 1200, 1920],
 		imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
 		formats: ["image/webp"],
 		minimumCacheTTL: 60,
