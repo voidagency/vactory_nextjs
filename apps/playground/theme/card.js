3ecbec9d1d0457daca43c3ed3d4fbdfb384ab421
Mustapha BKL <m.boukali@void.fr>
2022-04-02 11:05:21 +0000

    style: add custom theme object override for heading and cards

---
+++ apps/playground/theme/card.js
@@ -0,0 +1,20 @@
+export const card = {
+	default: {
+		card: "flex flex-col-reverse shadow-lg bg-white overflow-hidden text-indigo-500",
+		image: "flex-shrink-0",
+		body: "px-5 py-5",
+		tag: "text-xl font-bold text-indigo-600",
+		title: "text-xl font-semibold text-gray-900 text-indigo-600",
+		excerpt: "mt-3 text-base text-gray-500 dark:text-gray-400",
+		link: "text-indigo-500 border border-indigo-500 border-solid",
+	},
+	toto: {
+		card: "flex flex-col-reverse shadow-lg bg-red-500 overflow-hidden text-indigo-500",
+		image: "flex-shrink-0",
+		body: "px-5 py-5",
+		tag: "text-xl font-bold text-indigo-600",
+		title: "text-xl font-semibold text-gray-900 text-indigo-600",
+		excerpt: "mt-3 text-base text-gray-500 dark:text-gray-400",
+		link: "text-indigo-500 border border-indigo-500 border-solid",
+	},
+}
