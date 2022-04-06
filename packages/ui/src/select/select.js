b54f7ce38115e76d1322b48bd9b16ebdb15ec87a
abdedaoudi <a.daoudi@void.fr>
2022-04-06 17:52:51 +0000

    feat(ui): fix select UI

--- packages/ui/src/select/select.js
+++ packages/ui/src/select/select.js
@@ -1,43 +1,85 @@
-import React, { useContext } from "react"
-import clsx from "clsx"
-import PropTypes from "prop-types"
+import { Fragment, useState, useContext } from "react"
+import { Transition } from "@vactory/headlessui/transition"
+import { Listbox } from "@vactory/headlessui/listbox"
+import { Icon } from "@vactory/ui/icon"
 import { ThemeContext } from "../context/context"
+import clsx from "clsx"
 
-const Select = ({
-	className = "",
-	list = [],
-	variant = "default",
-	selected = "",
-	...props
-}) => {
-	const [selectedValue, handleChange] = React.useState(selected)
+export const Select = ({ list, chevronDownIcon, checkIcon, variant = "default" }) => {
+	const [selected, setSelected] = useState(list[0])
 	const { select } = useContext(ThemeContext)
-	React.useState(() => {
-		if (!selected) {
-			handleChange(list[0].value)
-		}
-	})
 	return (
-		<select
-			className={clsx(select[variant], className)}
-			value={selectedValue}
-			defaultValue={selectedValue}
-			{...props}
-		>
-			{list.map((item, index) => {
-				return (
-					<option key={index} value={item.value}>
-						{item.content}
-					</option>
-				)
-			})}
-		</select>
+		<div className={clsx(select[variant].wrapper)}>
+			<Listbox value={selected} onChange={setSelected}>
+				<div className={select[variant].listBox.wrapper}>
+					<Listbox.Button className={select[variant].listBox.button.wrapper}>
+						<span className={select[variant].listBox.button.selected}>
+							{selected.value}
+						</span>
+						<span className={select[variant].listBox.button.selectedIcon}>
+							{chevronDownIcon && chevronDownIcon}
+						</span>
+					</Listbox.Button>
+					<Transition
+						as={Fragment}
+						leave={select[variant].listBox.transition.leave}
+						leaveFrom={select[variant].listBox.transition.leaveFrom}
+						leaveTo={select[variant].listBox.transition.leaveTo}
+					>
+						<Listbox.Options
+							className={select[variant].listBox.transition.listBoxOptions.wrapper}
+						>
+							{list.map((list, index) => (
+								<Listbox.Option
+									key={index}
+									className={({ active }) =>
+										`${
+											select[variant].listBox.transition.listBoxOptions.listBoxOption
+												.wrapper
+										} ${
+											active
+												? select[variant].listBox.transition.listBoxOptions.listBoxOption
+														.active
+												: select[variant].listBox.transition.listBoxOptions.listBoxOption
+														.notActive
+										}`
+									}
+									value={list}
+								>
+									{({ selected }) => (
+										<>
+											<span
+												className={`${
+													select[variant].listBox.transition.listBoxOptions.listBoxOption
+														.content.wrapper
+												} ${
+													selected
+														? select[variant].listBox.transition.listBoxOptions
+																.listBoxOption.selectedfont
+														: select[variant].listBox.transition.listBoxOptions
+																.listBoxOption.notselectedfont
+												}`}
+											>
+												{list.value}
+											</span>
+											{selected ? (
+												<span
+													className={
+														select[variant].listBox.transition.listBoxOptions
+															.listBoxOption.content.selected
+													}
+												>
+													{checkIcon && checkIcon}
+												</span>
+											) : null}
+										</>
+									)}
+								</Listbox.Option>
+							))}
+						</Listbox.Options>
+					</Transition>
+				</div>
+			</Listbox>
+		</div>
 	)
 }
-
-Select.propTypes = {
-	list: PropTypes.array.isRequired,
-	selected: PropTypes.string,
-}
-
-export { Select }
