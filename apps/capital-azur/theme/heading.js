4d119f6dffd5a044d6ebe425eeee380b02c243b4
abdedaoudi <a.daoudi@void.fr>
2022-04-27 16:17:25 +0000

    feat(capital-azur): insights page

--- apps/capital-azur/theme/heading.js
+++ apps/capital-azur/theme/heading.js
@@ -2,4 +2,5 @@ export const heading = {
 	1: `text-sm`,
 	2: `text-xl font-medium leading-7 font-helvetica-400 lg:text-3xl lg:leading-8 font-bold `,
 	5: `text-base font-normal leading-4 font-helvetica-400 lg:text-lg lg:leading-5`,
+	6: `text-3xl font-helvetica-400`,
 }
---
+++ apps/capital-azur/widgets/insights-header/data.jsx
@@ -0,0 +1,3 @@
+export const data = {
+	title: "Actualités, Analyses et points de vue de nos experts",
+}
