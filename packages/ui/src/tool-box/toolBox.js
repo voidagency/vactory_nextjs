e853b5a45f8ceae6775607d19266518db084db0b
abdedaoudi <a.daoudi@void.fr>
2022-05-09 11:09:44 +0000

    feat(ui): stressing toolbox uit

--- packages/ui/src/tool-box/toolBox.js
+++ packages/ui/src/tool-box/toolBox.js
@@ -2,25 +2,33 @@ import { Button } from "@vactory/ui/button"
 import { useContext } from "react"
 import { ThemeContext } from "@vactory/ui/theme-context"
 import clsx from "clsx"
+import { Link } from "@vactory/ui/link"
 
-export const ToolBox = ({ list, className = "", variant = "default" }) => {
+export const ToolBox = ({ list, className = "", variant }) => {
 	const { toolbox } = useContext(ThemeContext)
 	return (
 		<div className={clsx(toolbox[variant].wrapper, className)}>
-			<div className={toolbox[variant].container}>
-				{list.map((tool, index) => {
-					return (
-						<Button
-							key={index}
-							variant={toolbox[variant].button.variant}
-							size={toolbox[variant].button.size}
-							icon={tool.icon}
-						>
-							{tool.name}
-						</Button>
-					)
-				})}
-			</div>
+			<nav className={clsx(toolbox[variant].position, className)}>
+				<ul className={toolbox[variant].items}>
+					{list.map((item, index) => {
+						return (
+							<li key={item.id}>
+								<Link
+									key={index}
+									href={item.href}
+									className={toolbox[variant].item.wrapper}
+									variant={variant}
+								>
+									<div className={toolbox[variant].item.itemIcon}>{item.icon}</div>
+									<div className={toolbox[variant].item.itemTitle}>
+										<span className="block m-2">{item.title}</span>
+									</div>
+								</Link>
+							</li>
+						)
+					})}
+				</ul>
+			</nav>
 		</div>
 	)
 }
